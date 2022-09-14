55901772b0283502138c1eca9730ae83a75a4421
Casey Glidewell <caseyglidewell@gmail.com>
2022-09-14 15:55:02 -0700

    Quiz question 6 and 7 fully implemented to meet the javascript side of the lab 3 requirements. That was a doozy.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 'use strict';
-
+let score = 0;
 let visitorName = prompt('What\'s your name?');
+let guessednum = 0;
+let flavorguess = 'NONE';
+let correct = false;
+let numguesses = 0;
+let flavguesses = 0;
 
-/*
-Answers are Yes, No, Yes, Yes, No
-*/
+const flavors = ['peanutbutter', 'mintchocolatechip', 'mint', 'chocolatechip', 'mintchoco', 'mintchocochip', 'bacon', 'cookiesandcream', 'cookiesncream'];//all "Correct" Icecream answers
+
+
+//Answers are Yes, No, Yes, Yes, No
+
+
 
 alert('Welcome ' + visitorName + '! I\'m going to give you some yes or no questions, try and guess these facts about me!');
 
@@ -13,6 +21,7 @@ let resp1 = prompt('Is Casey\'s favorite breakfast food Hashbrowns?');
 if (resp1.toUpperCase().charAt(0) === 'Y') {
 
   alert('That\'s right! Casey absolutely loves hashbrowns with just some salt and pepper.');
+  score++;
 }
 else {
 
@@ -26,6 +35,7 @@ if (resp2.toUpperCase().charAt(0) === 'N') {
 
 
   alert('That\'s right! Casey has been growing up around, and playing with computers as young as the age of 4. She remembers enjoying playing games on the original Macintosh, such as Cosmic Osmo!');
+  score++;
 }
 
 else {
@@ -39,6 +49,7 @@ let resp3 = prompt('Has Casey published and released to the public any videogame
 if (resp3.toUpperCase().charAt(0) === 'Y') {
 
   alert('That\'s right! Casey has released to itch.io three free prototype videogames, titled: Here\'s The Kicker, The Jackasstronaut, and Nest Defender The First: The Start Of An Empire.');
+  score++;
 }
 
 else {
@@ -51,6 +62,7 @@ let resp4 = prompt('Does Casey value experimental games that ultimately fail to 
 if (resp4.toUpperCase().charAt(0) === 'Y') {
 
   alert('That\'s right! Casey is far more interested in weird and experimental games that try something new, even if the final result is something that is entirely not fun. Yume Nikki is a favorite, as it uses a tool for making RPGs, and instead uses it to make an objectiveless, surreal exploration game.');
+  score++;
 }
 
 else {
@@ -63,6 +75,7 @@ let resp5 = prompt('Does Casey consider themselves to be an individual human?');
 if (resp5.toUpperCase().charAt(0) === 'N') {
 
   alert('That is actually correct! While Casey considers themselves the "main person" within their body, smaller other personalities sometimes chime in to provide secondary opinions. These others have their own creeds and opinions, and even preferred names, seperate from my own. Brains are wild.');
+  score++;
 }
 
 else {
@@ -70,4 +83,83 @@ else {
   alert('That is, oddly enough, incorrect! While Casey considers themselves the "main person" within their body, smaller other personalities sometimes chime in to provide secondary opinions. These others have their own creeds and opinions, and even preferred names, seperate from Casey\'s. Brains are wild.');
 }
 
-alert('Thank you for playing along, ' + visitorName + '. Maybe you feel that you understand me just that little bit better.');
+
+
+alert('Guess my number below 11, and above 0! You have 4 tries. Ready?');
+
+guessednum = prompt('Go on, guess!');
+numguesses++;
+
+while (!correct) {
+
+
+  if (Number(guessednum) === 4) {
+    alert('you got it in ' + numguesses + '!');
+    numguesses++;
+    score++;
+    correct = true;
+  }
+
+  if (numguesses > 3) {
+    alert('Sorry! Out of guesses! the correct answer was 4.');
+    break;
+  }
+
+  if (Number(guessednum) > 4) {
+    numguesses++;
+    guessednum = prompt('Too high!');
+
+  }
+
+  else if (Number(guessednum) < 4) {
+    numguesses++;
+    guessednum = prompt('too low!');
+
+  }
+
+
+}
+
+correct = false; //reset the Correct bool that way the next one isn't skipped.
+
+alert('Try and guess one of my favorite ice cream flavors! You have 6 tries. Ready?');
+
+
+while (flavguesses < 6 && !correct) {
+
+  flavorguess = prompt('guess a flavor!');
+  console.log(`adjusted guessed flavor>>${flavorguess.toLowerCase().replace(/\s+/g, '')}`)
+
+  for (let i = 0; i < flavors.length; i++) {
+    console.log(`array flavor>> ${flavors[i]}`);
+    if (flavors[i] === flavorguess.toLowerCase().replace(/\s+/g, '')) {
+      alert('that\'s right! You got one!');
+      correct = true;
+      score++;
+    }
+
+  }
+
+  if (!correct) {
+    alert('nope, keep guessing!');
+    flavguesses++;
+  }
+
+}
+
+
+let flavorlist = " ";
+
+for (let i = 0; i < flavors.length; i++) {
+
+  flavorlist += flavors[i];
+  flavorlist += ', ';
+  console.log(flavors[i]);
+  console.log(flavorlist);
+}
+
+alert('The possible correct answers were' + flavorlist + '.');
+
+
+
+alert('Thank you for playing, ' + visitorName + '. Maybe you feel that you understand me just that little bit better.\n\nBy the way, your score was ' + score + '.');
